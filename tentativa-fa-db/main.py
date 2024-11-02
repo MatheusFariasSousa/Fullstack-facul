c09ec6b5d9e086257bd00c277b131a26ef9362da
Matheus Farias de Sousa <143126173+MatheusFariasSousa@users.noreply.github.com>
2024-11-02 18:24:53 -0300

    Add files via upload

---
+++ tentativa-fa-db/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from routes.user_route import router
+from routes.product_route import client
+from routes.sale_route import rota
+from routes.front_route import front_router
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware 
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+)
+
+@app.get("/health-check")
+def health_check():
+    return "works"
+
+app.include_router(router=router)
+app.include_router(router=client)
+app.include_router(router=rota)
+app.include_router(router=front_router)
+
+
+
+
+
+app.mount("/static",StaticFiles(directory="static"),name="static")
+
+
+
---
+++ tentativa-fa-db/routes/front_route.py
@@ -0,0 +1,68 @@
+from fastapi import APIRouter,Form,Depends,Response,status,HTTPException
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from sqlalchemy.orm import Session
+from typing import List
+from routes.deps import get_conection
+from use_cases.user_use_case import User_use_cases
+from passlib.context import CryptContext
+from schema.user_schema import User_schema,User_Schema_Front
+from db.model import User
+
+front_router = APIRouter(prefix="/front",tags=["Front"])
+
+crypt =CryptContext(schemes=["sha256_crypt"])
+
+
+
+templates = Jinja2Templates(directory="templates")
+
+"""@front_router.get("/")
+def read_front(request:Request)a:
+    return templates.TemplateResponse(request=request,name="index.html")"""
+
+@front_router.get("/sucesso")
+def success_page(request: Request):
+    return templates.TemplateResponse("sucesso.html", {"request": request})
+
+@front_router.post("/")
+def post_front(db_session:Session = Depends(get_conection),nome:str=Form(...),cpf:str=Form(...),senha:str=Form(...),email:str=Form(...)):
+    person = User_schema(name=nome,email=email,password=senha,cpf_cnpj=cpf,is_active=True)
+    print("1")
+    uc = User_use_cases(db_session=db_session)
+    uc.post_user(person)
+    
+
+"""@front_router.get("/users-page")
+def get_page(request:Request):
+    return templates.TemplateResponse("users.html",{"request":request})"""
+
+@front_router.get("/users-page", response_model=List[User_Schema_Front])
+def get_users(db_session: Session = Depends(get_conection)):
+    users = db_session.query(User).all()
+    
+    return users 
+
+@front_router.post("/put-user")
+def put_user(db_session:Session = Depends(get_conection),name:str=Form(...),id:int=Form(...),password:str=Form(...),email:str=Form(...)):
+    person = db_session.query(User).where(User.id == id).first()
+    
+    person.email = email
+    person.name = name
+    person.password = password
+    db_session.add(person)
+    try:
+        db_session.commit()
+        return RedirectResponse(url="/front/users-page", status_code=303)
+
+    except:
+        return "Erro!"
+
+
+    
+@front_router.get("/testando")
+def get_teste(request:Request):
+    return {"message":"welcome to your todo list"}
+    
+
